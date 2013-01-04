c17c647e09933b7b1c58787b046338d3502fed5c
Ricardo Alcocer <alco@ricardoalcocer.com>
2013-01-03 18:32:51 -0800

    Menu, click and displaying item list working properly

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,9 @@
 var currentOrientation="PORTRAIT";
 var menuOpen = false;
+var baseURL="http://query.yahooapis.com/v1/public/yql?";
+
+var XHR = require("xhr");
+var xhr = new XHR();
 var COL = require("mod_col");
 var col = new COL();
 
@@ -40,24 +44,55 @@ var loadCategoriesMenu=function(){
 var onCategoryClick=function(e){
 	// according to http://developer.appcelerator.com/question/133642/tableview---erowdatasomething-vs-erowsomething
 	// row should be used to reach row info instead of rowData
-	var selectedCategoryId=e.row.categoryId;
-	var selectedCategoryName=e.row.categoryName;
 	
-	loadData(selectedCategoryName); // this should send a JSON string to the function
+	// we need to check the event source, in case the user clicked on the label and not the row
+	if(e.source.id==='categoryEntry'){
+		var selectedCategoryId=e.source.parent.categoryId;
+		var selectedCategoryName=e.source.parent.categoryName;
+	}else{
+		var selectedCategoryId=e.row.categoryId;
+		var selectedCategoryName=e.row.categoryName;
+	}
+	
+	var data={
+		id:selectedCategoryId,
+		caption:selectedCategoryName
+	}
+	$.currentcategory.text=data.caption;
+	loadData(data,0); // this should send a JSON string to the function
 }
 
 // ################################################################
-var loadData=function(args){
-	var tableData=[];
-	for (var i=0;i<=10;i++){
-		var payload={
-			itemId:i,
-			itemName:args + ' - Item ' + i
-		}
-		var row=Alloy.createController('itemrow',payload).getView();
-		tableData.push(row);
+var loadData=function(args,startAt){
+	var httpOptions={
+		async				:true,
+		ttl					:false, 
+		shouldAuthenticate	:false,
+		contentType			: "application/json"
 	}
-	$.mainList.data=tableData;
+	
+	var onSuccessCallback=function(jsonData){
+		var dataArray=col.getDataArray(jsonData,startAt);
+		var contentLength=dataArray.length;
+		var tableData=[];
+		for (var i=0;i<contentLength;i++){
+			var payload={
+				itemId:dataArray[i].id,
+				itemName:dataArray[i].name
+			}
+			
+			var row=Alloy.createController('itemrow',payload).getView();
+			tableData.push(row);
+		}
+		$.mainList.data=tableData;
+		//$.mainList.scrollToTop();
+	}
+	
+	var onErrorCallback=function(data){
+		output="error";
+	}
+	
+	xhr.get(baseURL + "q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fclasificadosonline.com%2Fm%2FMiscellaneosListingM.asp%3FMisCat%3D" + args.id + "%26Submit2%3DSearch%2B-%2BBusqueda%26keyword%3D%26Desc%3D%26offset%3D" + startAt + "%22%20and%0A%20%20%20%20%20%20xpath%3D'%2Fhtml%2Fbody%2Fdiv%2Fdiv%2Fdiv%2Fform%2Ftable'&format=json&diagnostics=true&callback=", onSuccessCallback, onErrorCallback,httpOptions);
 }
 
 // ################################################################
@@ -69,5 +104,9 @@ Ti.Gesture.addEventListener('orientationchange', function(e) {
 
 // ################################################################
 loadCategoriesMenu();
-loadData('Todas/All');
+loadData({
+		id:0,
+		caption:'Todas/All'
+	},100
+);
 $.index.open();
